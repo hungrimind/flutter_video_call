d1240d39e415045d80329e2cc2ff727694a35aa1
Tadas Petra <tadas@tadaspetra.com>
2024-02-07 21:02:54 -0600

    Simplify http import

--- lib/token.dart
+++ lib/token.dart
@@ -2,12 +2,12 @@ import "dart:convert";
 
 import "package:flutter/material.dart";
 import "package:flutter_video_call/consts.dart";
-import "package:http/http.dart" as http;
+import "package:http/http.dart";
 
 Future<String> fetchToken(int uid, String channelName) async {
   String url = '$tokenUrl/rtc/$channelName/1/uid/${uid.toString()}?expiry=45';
 
-  final response = await http.get(Uri.parse(url));
+  final response = await get(Uri.parse(url));
 
   if (response.statusCode == 200) {
     Map<String, dynamic> json = jsonDecode(response.body);
